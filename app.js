4dfce92840ee88aa7c00f5128495ecae90725fb4
adriannicolai <anlumbad@gmail.com>
2022-09-18 17:23:51 +0800

    added the router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express    = require('express');
 const app        = express();
 const ejs        = require('ejs');
 const bodyParser = require('body-parser');
+const Router     = require('./routes');
 
 const server  = app.listen(process.env.PORT || 3000, function(){
     console.log('listening at 3000!')
