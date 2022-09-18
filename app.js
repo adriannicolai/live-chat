4637bbe6707671ae772f2e6a470af5c52254ee91
adriannicolai <anlumbad@gmail.com>
2022-09-18 17:13:51 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express    = require('express');
+const app        = express();
+const ejs        = require('ejs');
+const bodyParser = require('body-parser');
+
+const server  = app.listen(process.env.PORT || 3000, function(){
+    console.log('listening at 3000!')
+});
+
+const io = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/assets'));
+
+app.use(Router);
