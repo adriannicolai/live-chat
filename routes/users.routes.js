b3b61f328654a6190ea64d4b6a4f138602120f2f
adriannicolai <anlumbad@gmail.com>
2022-09-19 16:16:16 +0800

    updated typo

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,13 +4,24 @@ const UsersController = require('../controllers/users.controller');
 
 /**
 * DOCU: Route to show home page. <br>
-* When user navigaes to the webstite. <br>
+* When user navigates to the website. <br>
 * Triggered by: n/a <br>
-* Last updated at: September 18, 2022
+* Last updated at: September 19, 2022
 * @function
 * @memberof module:routes/users~UserRoute
 * @author Adrian
 */
 UserRoute.get("/home", UsersController.home);
+
+/**
+* DOCU: Route to show the chat page for user. <br>
+* When user navigates to the website. <br>
+* Triggered by: n/a <br>
+* Last updated at: September 19, 2022
+* @function
+* @memberof module:routes/users~UserRoute
+* @author Adrian
+*/
+UserRoute.get("/help", UsersController.help);
 
 module.exports = UserRoute;
